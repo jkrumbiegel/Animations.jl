9e6163c674137e335d7c8928fb344d8a82d69bd9
Julius Krumbiegel <juliuskrumbiegel@Julius-MacBook-Pro.local>
2019-08-01 09:36:54 -0700

    yoyo / repeat w/o functionality

--- src/Animations.jl
+++ src/Animations.jl
@@ -2,13 +2,22 @@ module Animations
 
 import Observables
 
-export Easing, LinearEasing, SineEasing, Animation, Keyframe, add!, update!, linear_interpolate
+export Easing, EasingType, LinearEasing, SineEasing, Step, Animation, Keyframe, add!, update!, linear_interpolate
 
-abstract type Easing end
+abstract type EasingType end
 
-struct LinearEasing <: Easing end
-struct SineEasing <: Easing end
-struct StepEasing <: Easing end
+#TODO yoyo, repeats, all that good stuff
+struct LinearEasing <: EasingType end
+struct SineEasing <: EasingType end
+struct Step <: EasingType end
+
+struct Easing
+    type::Type{<:EasingType}
+    yoyo::Bool
+    ntimes::Int
+end
+
+Easing(;type=Step(), yoyo=false, ntimes=1) = Easing(type, yoyo, ntimes)
 
 struct Keyframe{T}
     t::Float64
@@ -18,7 +27,7 @@ end
 Keyframe(t::Real, value) = Keyframe(convert(Float64, t), value)
 
 struct Animation{T}
-    observable::Observables.Observable{T}
+    obs::Observables.Observable{T}
     frames::Vector{Keyframe{T}}
     easings::Vector{Easing}
 
@@ -44,10 +53,17 @@ struct Animation{T}
         types = unique(map(x -> typeof(x), differenttype_kfs))
         error("""All keyframes need the same parametric type. Types are:\n$types""")
     end
+
+    function Animation(kfs::Vector{Keyframe{T}}, easing::Easing) where T
+        if length(kfs) <= 1
+            error("There must be at least two keyframes.")
+        end
+        Animation(kfs, [easing for _ in 1:length(kfs) - 1])
+    end
 end
 Base.Broadcast.broadcastable(a::Animation) = Ref(a)
 
-Observables.on(f::Function, a::Animation) = Observables.on(f, a.observable)
+Observables.on(f::Function, a::Animation) = Observables.on(f, a.obs)
 # Observables.map(a::Animation, f::Function) =
 
 function update!(a::Animation, t::Real)
@@ -58,15 +74,15 @@ function update!(a::Animation, t::Real)
 
     if isnothing(i_first_after_t)
         # t after last keyframe
-        a.observable[] = a.frames[end].value
+        a.obs[] = a.frames[end].value
     elseif i_first_after_t == 1
         # t before first keyframe
-        a.observable[] = a.frames[1].value
+        a.obs[] = a.frames[1].value
     else
         # t between two keyframes
         i_from = i_first_after_t - 1
         i_to = i_first_after_t
-        a.observable[] = interpolate(a.easings[i_from], t, a.frames[i_from], a.frames[i_to])
+        a.obs[] = interpolate(a.easings[i_from], t, a.frames[i_from], a.frames[i_to])
     end
 end
 
@@ -89,14 +105,27 @@ function validate_keyframe_times(kfs::Vector{Keyframe{T}}) where T
 end
 
 function interpolate(easing::Easing, t::Real, k1::Keyframe{T}, k2::Keyframe{T}) where T
-    if t <= k1.t
+
+    time_fraction = (t - k1.t) / (k2.t - k1.t)
+
+    if time_fraction <= 0
         return k1.value
-    elseif t >= k2.t
+    elseif time_fraction >= 1
         return k2.value
     end
 
-    time_fraction = (t - k1.t) / (k2.t - k1.t)
-    interp_ratio = interpolation_ratio(easing, time_fraction)
+    repeated_time_fraction = mod(time_fraction * easing.ntimes + 1, easing.ntimes)
+
+    interp_ratio = interpolation_ratio(easing.type, repeated_time_fraction)
+
+    # these checks enable to return early if values are 0 or 1, which is why
+    # step EasingType can be used for non-interpolateable values like strings
+    if interp_ratio <= 0
+        return k1.value
+    elseif interp_ratio >= 1
+        return k2.value
+    end
+
     interp_value = linear_interpolate(interp_ratio, k1.value, k2.value)
 end
 
@@ -105,16 +134,16 @@ function linear_interpolate(fraction::Real, value1::T, value2::T) where T
     (value2 .- value1) .* fraction .+ value1
 end
 
-function interpolation_ratio(easing::SineEasing, fraction)
+function interpolation_ratio(::Type{SineEasing}, fraction)
     return sin(pi * fraction - 0.5pi) * 0.5 + 0.5
 end
 
-function interpolation_ratio(easing::LinearEasing, fraction)
+function interpolation_ratio(::Type{LinearEasing}, fraction)
     return fraction
 end
 
-function interpolation_ratio(easing::StepEasing, fraction)
-    return fraction <= 0.5 ? 0 : 1
+function interpolation_ratio(::Type{Step}, fraction)
+    return fraction < 0.5 ? 0 : 1
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,17 +7,17 @@ using Colors: Color, RGB, weighted_color_mean
 
     kf1 = Keyframe(1, 5.0)
     kf2 = Keyframe(3, 10.0)
-    kf3 = Keyframe(5, 0.0)
+    kf3 = Keyframe(6, 0.0)
 
     animation = Animation(
         [kf1, kf2, kf3],
-        Easing[SineEasing(), SineEasing()]
+        [Easing(type=SineEasing), Easing(type=LinearEasing)]
     )
 
     on(animation) do x
         println(x)
     end
-    update!.(animation, [0, 1, 2, 3, 4, 5, 6])
+    update!.(animation, [0, 1, 2, 3, 4, 5, 6, 7])
 end
 
 @testset "vector interpolate" begin
@@ -27,7 +27,7 @@ end
 
     animation = Animation(
         [kf1, kf2],
-        Easing[SineEasing()]
+        Easing(type=SineEasing)
     )
 
     on(animation) do x
@@ -44,7 +44,7 @@ end
 
     animation = Animation(
         [kf1, kf2],
-        Easing[SineEasing()]
+        Easing(type=SineEasing)
     )
 
     on(animation) do x
@@ -55,5 +55,24 @@ end
     Animations.linear_interpolate(fraction::Real, c1::Color, c2::Color) = weighted_color_mean(fraction, c1, c2)
 
     update!.(animation, [0, 0.25, 0.5, 0.75, 1])
+
+end
+
+@testset "string steps" begin
+
+    kf1 = Keyframe(0, "first")
+    kf2 = Keyframe(1, "second")
+    kf3 = Keyframe(2, "third")
+
+    animation = Animation(
+        [kf1, kf2, kf3],
+        Easing(type=Step)
+    )
+
+    on(animation) do x
+        println(x)
+    end
+
+    update!.(animation, [0, 0.5, 1, 1.5, 2])
 
 end
