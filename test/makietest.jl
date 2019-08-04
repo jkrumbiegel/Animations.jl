7c68c8837aa1e8d11a4fdc720f49b01a7edc5821
Julius Krumbiegel <juliuskrumbiegel@Julius-MacBook-Pro.local>
2019-08-04 11:10:49 -0700

    bigger example

--- test/makietest.jl
+++ test/makietest.jl
@@ -1,24 +1,86 @@
 using Revise
 using Makie
 using Animations
+using Printf
+using Colors
+
+Animations.linear_interpolate(fraction::Real, c1::Color, c2::Color) = weighted_color_mean(1 - fraction, c1, c2)
 
 function test()
-    a = Animations.Animation(
-        @timestamps(0, :2),
-        [[1. 1], [2. 2]],
-        Easing(easing=SineIOEasing(), ntimes=9, yoyo=true)
+
+    animations = []
+
+    push!(animations, Animations.Animation(
+        [0, 3],
+        [[0. 0.], [0. 1.]],
+        Easing(easing=LinearEasing(), ntimes=1, yoyo=false, prewait=0.0, postwait=0.0)
+    ))
+
+    push!(animations, Animations.Animation(
+        [0, 3],
+        [[1. 0.], [1. 1.]],
+        Easing(easing=NoEasing(), ntimes=1, yoyo=false, prewait=0.0, postwait=0.0)
+    ))
+
+    push!(animations, Animations.Animation(
+        [0, 3],
+        [[2. 0.], [2. 1.]],
+        Easing(easing=StepEasing(), ntimes=1, yoyo=false, prewait=0.0, postwait=0.0)
+    ))
+
+    push!(animations, Animations.Animation(
+        [0, 3],
+        [[3. 0.], [3. 1.]],
+        Easing(easing=SineIOEasing(), ntimes=1, yoyo=false, prewait=0.0, postwait=0.0)
+    ))
+
+    push!(animations, Animations.Animation(
+        [0, 3],
+        [[4. 0.], [4. 1.]],
+        Easing(easing=SineIOEasing(), ntimes=3, yoyo=false, prewait=0.0, postwait=0.0)
+    ))
+
+    push!(animations, Animations.Animation(
+        [0, 3],
+        [[5. 0.], [5. 1.]],
+        Easing(easing=SineIOEasing(), ntimes=3, yoyo=false, prewait=0.2, postwait=0.2)
+    ))
+
+    push!(animations, Animations.Animation(
+        [0, 3],
+        [[6. 0.], [6. 1.]],
+        Easing(easing=SineIOEasing(), ntimes=3, yoyo=true, prewait=0.0, postwait=0.0)
+    ))
+
+    push!(animations, Animations.Animation(
+        [0, 3],
+        [[7. 0.], [7. 1.]],
+        Easing(easing=SineIOEasing(), ntimes=3, yoyo=true, prewait=0.2, postwait=0.2)
+    ))
+
+    color_animation = Animations.Animation(
+        [0, 3],
+        [LCHab(70, 70, 0), LCHab(70, 70, 360)],
+        Easing(easing=LinearEasing())
     )
 
-    println(a.obs)
+    width = length(animations) - 1
 
-    pos = lift(p -> p, a.obs)
+    scene = Scene(resolution=(600, 600))
+    for a in animations
+        scatter!(scene, a.obs, markersize=0.2, color=color_animation.obs)
+    end
 
-    scene = Scene(resolution=(500, 500))
-    scat = scatter!(scene, pos)[end]
-    scene
+    translate!(lines!(scene, [0, width], [0, 0])[end], (0, 0, -1))
+    translate!(lines!(scene, [0, width], [1, 1])[end], (0, 0, -1))
 
-    record(scene, "test.mp4", 0:1/60:2) do t
-        Animations.update!(a, t)
+    timestamp = Node("0")
+    text!(scene, timestamp, position=Point2f0(width / 2, 1.2), align = (:center,  :center), textsize = 0.4)
+
+    record(scene, "test.gif", -1:1/25:4; framerate=25) do t
+        Animations.update!.(animations, t)
+        Animations.update!(color_animation, t)
+        timestamp[] = @sprintf "t = %.2f" t
     end
     nothing
 end
