921ed25e13e41072340b02697d83ac057fc81795
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2019-10-08 09:50:26 +0200

    example

---
+++ misc/example.jl
@@ -0,0 +1,43 @@
+using Revise
+using Layered
+using Animations
+using Colors
+
+
+function test()
+
+    duration = 6
+
+    a_y = Animation(
+        0, 1,
+        polyout(2; n=8, yoyo=true),
+        duration, 0)
+
+    a_x = Animation(
+        0, 0,
+        linease(n=2, yoyo=true),
+        duration, 1)
+
+    a_color = Animation(
+        0, RGB(0.251, 0.388, 0.847),
+        duration * 0.25, RGB(0.22, 0.596, 0.149),
+        duration * 0.5, RGB(0.584, 0.345, 0.698),
+        duration * 0.75, RGB(0.796, 0.235, 0.2),
+        defaulteasing=noease()
+    )
+
+
+    function frame(t)
+        c, tl = canvas(2, 2)
+
+        l = layer_in_rect!(tl, c.rect, :w, :norm, margin=30)
+
+        circle!(l, P(a_x(t), a_y(t)), 0.15) + Fill(a_color(t)) + Stroke(nothing)
+
+        c
+
+    end
+
+    record(frame, "test.gif", 30, duration)
+
+end; test()
